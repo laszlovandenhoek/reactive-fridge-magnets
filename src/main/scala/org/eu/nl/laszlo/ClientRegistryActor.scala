0524d33e83875610ff05c3dbf43b75fc6c000ff8
László van den Hoek <laszlo.vandenhoek@gmail.com>
2018-09-13 23:10:38 +0200

    FridgeActor first version done (untested)

--- src/main/scala/org/eu/nl/laszlo/ClientRegistryActor.scala
+++ src/main/scala/org/eu/nl/laszlo/ClientRegistryActor.scala
@@ -1,8 +1,13 @@
 package org.eu.nl.laszlo
 
+import java.util.UUID
+
 import akka.actor.{ Actor, ActorLogging, Props }
 
 object ClientRegistryActor {
+  //this should be an Actor of its own
+  final case class ConnectedClient(handle: UUID, nickname: String)
+
   def props: Props = Props[ClientRegistryActor]
 }
 
--- src/main/scala/org/eu/nl/laszlo/FridgeActor.scala
+++ src/main/scala/org/eu/nl/laszlo/FridgeActor.scala
@@ -2,10 +2,13 @@ package org.eu.nl.laszlo
 
 import java.util.UUID
 
-import akka.actor.{ Actor, ActorLogging, ActorRef, Props }
+import akka.actor.{Actor, ActorLogging, ActorRef, Props}
 
-//this one should move to ClientRegistry once it's created
-final case class ConnectedClient(handle: UUID, nickname: String)
+object Magnet {
+  def apply(text: CharSequence): Magnet = {
+    Magnet(UUID.randomUUID(), text.toString)
+  }
+}
 
 final case class Magnet(handle: UUID, text: String)
 
@@ -13,25 +16,35 @@ final case class Point(x: Int, y: Int)
 
 object FridgeActor {
 
+  final val canvasSizeHorizontal: Int = 1000
+  final val canvasSizeVertical: Int = 1000
+
+  private def createMagnets(): Set[Magnet] = {
+    Set('a' to 'z').map(Magnet.apply(_))
+  }
+
+  private def withinBounds(point: Point): Boolean =
+    point.x >= 0 && point.x < canvasSizeHorizontal &&
+      point.y >= 0 && point.y < canvasSizeVertical
+
+  def props(clientRegistry: ActorRef): Props = Props[FridgeActor](new FridgeActor(clientRegistry))
+
   //requests
   final case object GetFullState
 
-  final case class GrabMagnet(magnet: Magnet, grabber: ConnectedClient)
+  final case class GrabMagnet(magnet: Magnet)
 
   final case class DragMagnet(magnet: Magnet, toPoint: Point)
 
-  final case class ReleaseMagnet(magnet: Magnet) //check if ConnectedClient == sender() ?
+  final case class ReleaseMagnet(magnet: Magnet)
 
   //responses
-  final case class NewState(magnets: Set[Magnet], partial: Boolean)
+  final case class NewState(positions: Map[Magnet, Point], partial: Boolean)
 
-  final case class MagnetGrabbed(magnet: Magnet, grabber: ConnectedClient)
-
-  final case class MagnetsMoved(magnets: Set[Magnet])
+  final case class MagnetGrabbed(magnet: Magnet, grabber: ActorRef) //TODO: this could be a typed ActorRef, I think
 
   final case class MagnetReleased(magnet: Magnet)
 
-  def props(clientRegistry: ActorRef) = Props[FridgeActor](new FridgeActor(clientRegistry))
 }
 
 class FridgeActor(clientRegistry: ActorRef) extends Actor with ActorLogging {
@@ -39,33 +52,46 @@ class FridgeActor(clientRegistry: ActorRef) extends Actor with ActorLogging {
   import FridgeActor._
 
   val magnets: Set[Magnet] = createMagnets()
-  var draggedBy: Map[Magnet, ConnectedClient] = Map.empty
+  var draggers: Map[ActorRef, Magnet] = Map.empty
   var positions: Map[Magnet, Point] = magnets.map(m => (m, Point(0, 0))).toMap
 
   def receive: Receive = {
     case GetFullState =>
-      sender() ! NewState(magnets, false)
-    case GrabMagnet(magnet, hopeful) =>
-      draggedBy.get(magnet) match {
-        case None =>
-          broadcast(MagnetGrabbed(magnet, hopeful))
-        case Some(`hopeful`) =>
-          sender() ! MagnetGrabbed(magnet, hopeful)
-        case Some(otherBastard) =>
-          sender() ! MagnetGrabbed(magnet, otherBastard)
+      sender() ! NewState(positions, partial = false)
+    case GrabMagnet(magnet) =>
+      val client = sender()
+      val clientDraggingMagnet = draggers.find({ case (_, m) => m == magnet }).map(_._1)
+      val draggedByClient = draggers.get(client)
+
+      (clientDraggingMagnet, draggedByClient) match {
+        case (None, None) => //client is not dragging anything, and this magnet is free
+          setDragger(client, magnet)
+        case (None, Some(otherMagnet)) => //client is already dragging a different magnet, but this magnet is free
+          log.warning("{} was already dragging {}; switching to {}", client, otherMagnet, magnet)
+          setDragger(client, magnet)
+        case (Some(`client`), Some(`magnet`)) => //client is already dragging this magnet
+          client ! MagnetGrabbed(magnet, client) //no need for a separate case object AlreadyDragging, just a friendly reminder
+        case (Some(otherClient), _) => //some other bastard got it first :(
+          client ! MagnetGrabbed(magnet, otherClient)
       }
     case DragMagnet(magnet, toPoint) =>
-      ???
-    case ReleaseMagnet(name) =>
-      ???
+      if (draggers.get(sender()).contains(magnet) && withinBounds(toPoint)) {
+        positions = positions.updated(magnet, toPoint)
+        broadcast(NewState(positions, partial = true))
+      }
+    case ReleaseMagnet(magnet) =>
+      val client = sender()
+      if (draggers.get(client).contains(magnet))
+        draggers = draggers - client
+
   }
 
-  def broadcast(message: Any): Unit = {
+  private def setDragger(connectedClient: ActorRef, magnet: Magnet): Unit = {
+    draggers = draggers.updated(connectedClient, magnet)
+    broadcast(MagnetGrabbed(magnet, connectedClient))
+  }
+
+  private def broadcast(message: Any): Unit = {
     clientRegistry ! message
   }
-
-  private def createMagnets(): Set[Magnet] = {
-    Set.empty
-  }
-
 }
